a935a927ad2beedb93c065992803355441d627bf
Brandon Truong <me@brandontruong.com>
2014-07-24 09:56:54 -0400

    First commit, very bare

---
+++ test/cutter.js
@@ -0,0 +1,25 @@
+var assert = require('assert'),
+	expect = require('chai').expect;
+
+describe('Cutter', function(){
+
+	describe('#words', function(){
+
+		it("parses words correctly", function(done){
+			expect('a').to.equal('a');
+			done();
+		});
+
+		it("parses words and count correctly", function(done){
+			expect('a').to.equal('a');
+			done();
+		});
+
+		it("parses paragraphs correctly", function(done){
+			expect('a').to.equal('a');
+			done();
+		});
+
+	});
+
+});
