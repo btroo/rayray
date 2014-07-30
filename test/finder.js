ed26826d841cb1aa6bd5f1e76c4ff8021a4af1a3
Brandon Truong <me@brandontruong.com>
2014-07-30 18:15:17 -0400

    finder updates, removed 'all' in functions, and rearranged files for finder merge

--- test/finder.js
+++ test/finder.js
@@ -1,4 +1,37 @@
 var assert = require('assert'),
+	_ = require('underscore-node'),
 	expect = require('chai').expect,
 	finder = require('../index').Finder;
 
+describe('Cutter', function(){
+	beforeEach(function(done){
+		passage = "I am a sentence. I am a group of words.\nI started a new paragraph";
+		done();
+	});
+
+	describe('#words', function(){
+		it("should parse words correctly", function(done){
+			var vocab = finder.getVocab(passage);
+			expect(vocab.length).to.equal(10);
+			done();
+		});
+
+		it("parses words and count correctly", function(done){
+			var vocab = finder.getVocabAndCount(passage);
+			expect(_.findWhere(vocab, {word: "a"}).count).to.equal(3);
+			done();
+		});
+	});
+
+	describe("#sentences", function(){
+		it("should parse sentences correctly", function(done){
+			// todo
+			done();
+		});
+	});
+
+	describe("#paragraphs", function(){
+
+	});
+
+});
